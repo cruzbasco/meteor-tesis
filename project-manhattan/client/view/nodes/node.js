f2b7df70aeacf56959f8aa9bd520eebc72f2f075
cruzbasco <cruzbasco@hotmail.com>
2015-06-04 15:39:10 -0400

    node is in this place
    refactor the code in node.js

--- project-manhattan/client/view/nodes/node.js
+++ project-manhattan/client/view/nodes/node.js
@@ -1,19 +1,6 @@
 /// <reference path="../../../lib/typings/meteor/meteor.d.ts"/>
 /// <reference path="../../../lib/typings/jquery/jquery.d.ts"/>
 
-var nodeArray;
-var idVar = 0;
-var middlePoint =[]
-
-Template.node.helpers({
-    'top': function () {
-        return middlePoint[0];
-    },
-    'left': function() {
-        return middlePoint[1];
-    }
-});
-
 Template.nodeForm.events({
 	'submit form': function (e) {
 		e.preventDefault();
@@ -21,73 +8,63 @@ Template.nodeForm.events({
 		var numberOfNodes = parseInt(e.target.numberLines.value);
 		e.target.numberLines.value= '';
 
-        var hasFather = e.target.hasFather.checked;
-        $("[name=hasFather]").removeAttr('checked');
-        
-        if (hasFather == true) {
-            numberOfNodes = numberOfNodes + 1;
-        }
-        
         numberOfNodes = numberOfNodes + 2;
-        
-        var heigthNav = $(".header").first().height();
-        var canvas = document.getElementById('node-canvas');
-        
-        canvas.width = $(window).width(); 
-        canvas.height = $(window).height() - heigthNav;
-       
-        nodeArray =[];
-       
-        var context = canvas.getContext('2d');
-        var centerX = canvas.width/2;
-        var centerY = canvas.height/2;
-        
-        middlePoint[0] = centerX;
-        middlePoint[1] = centerY;
-        
-        var r = 250;
-        var deg = 360 / numberOfNodes;
-        
-        for ( var i = 0 ; i < numberOfNodes ; i++ ) {
-        	context.lineWidth = 5;            
-            switch (i) {
-                case 0:
-                    context.strokeStyle = "#00F94F";
-                    context.setLineDash([3,4]);
-                    break;
-                case 1:
-                    context.strokeStyle = "#000000";
-                    break;
-                default:
-                    context.strokeStyle = "#FF4221";
-                    context.setLineDash([]);
-            }
-            
-            if (hasFather == true && (numberOfNodes-1) == i) {
-                context.strokeStyle = "#FF4221";                
-                context.lineWidth = 10;
-                context.setLineDash([]);
-            }
-            
-            context.beginPath();
-            context.moveTo(centerX, centerY);
-            var posX = centerX - (r * Math.cos(degToRad(deg * i)));
-            var posY = centerY - (r * Math.sin(degToRad(deg * i)));
-            
-            $( "#centerPoint" ).offset({ top: centerY, left: centerY});
-            
-            var colorLine = context.strokeStyle;
-            
-            nodeArray[i] = [i, posX, posY, colorLine];
-                        
-            
-            context.lineTo(posX, posY);
-            context.stroke();
-        }
-        
-        function degToRad(degree) {
-            var radians = (degree + 225)* (Math.PI/180);
-            return radians;
-        }	
+
+        drawLines (numberOfNodes);
 	}
 });
+
+function drawBubble (topBubble, leftBubble) {
+    var widthNode = ($(".node-dot").first().width())/2;
+    var heigthForm = $(".form-aux").first().height();
+    
+    $( "#centerPoint" ).offset({ 
+                top: topBubble + heigthForm,
+                left: leftBubble - widthNode});
+};
+
+function drawLines (numberOfNodes) {
+    var canvas = document.getElementById('node-canvas');
+    var heigthNav = $(".header").first().height();
+    
+    canvas.width = $(window).width(); 
+    canvas.height = $(window).height() - heigthNav;
+   
+    var context = canvas.getContext('2d');
+    var centerX = canvas.width/2;
+    var centerY = canvas.height/2;
+    var r = 250;
+    var deg = 360 / numberOfNodes;
+    
+    drawBubble(centerY+heigthNav, centerX);
+    
+    for ( var i = 0 ; i < numberOfNodes ; i++ ) {
+    	context.lineWidth = 5;            
+        switch (i) {
+            case 0:
+                context.strokeStyle = "#00F94F";
+                context.setLineDash([3,4]);
+                break;
+            case 1:
+                context.strokeStyle = "#000000";
+                break;
+            default:
+                context.strokeStyle = "#FF4221";
+                context.setLineDash([]);
+        }
+        
+        context.beginPath();
+        context.moveTo(centerX, centerY);
+        
+        var posX = centerX - (r * Math.cos(degToRad(deg * i)));
+        var posY = centerY - (r * Math.sin(degToRad(deg * i)));
+        
+        context.lineTo(posX, posY);
+        context.stroke();
+    }
+}
+
+function degToRad(degree) {
+        var radians = (degree + 225)* (Math.PI/180);
+        return radians;
+}
